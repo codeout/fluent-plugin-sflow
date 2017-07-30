4de6385fe394eed74763c2a1c8f15b8177457e5c
Shintaro Kojima <goodies@codeout.net>
2017-07-31 00:32:35 +0900

    bundle gem fluent-plugin-sflow

---
+++ lib/fluent/plugin/sflow.rb
@@ -0,0 +1,9 @@
+require "fluent/plugin/sflow/version"
+
+module Fluent
+  module Plugin
+    module Sflow
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent/plugin/sflow/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Sflow
+      VERSION = "0.1.0"
+    end
+  end
+end
